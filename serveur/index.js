49a451f657be2bbb901091963235592d3e2c891e
Baptiste <bmartin@enssat.fr>
2021-06-05 18:05:27 +0200

    Revert "v4.7"
    This reverts commit a555b890055fbe048d2a1bd53a792b2293554bf5.

--- serveur/index.js
+++ serveur/index.js
@@ -20,10 +20,6 @@ var corsOptions = {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//session
-app.use(session({secret: "shhh"}));
-var sess;
-
 app.post('/creerBot', cors(corsOptions), addNewBot);
 app.post('/inscription', cors(corsOptions), inscription);
 app.post('/connexion', cors(corsOptions), connexion);
@@ -100,12 +96,6 @@ async function connexion(req, res) {
     .then((result) => {
       if (result != null) {
         console.log(`index : user found : ${result.pseudo}`);
-
-        //session
-        sess=req.session;
-        sess.pseudo=pseudo;  
-        console.log(sess.pseudo);
-
         res.status(200).json({
           "status": "ok",
           "isAdmin": result.isAdmin,
